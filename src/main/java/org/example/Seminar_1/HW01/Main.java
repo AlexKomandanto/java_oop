059c9eaf20f43b519bf1a439139b36ab4f735d08
alexkom <komandato666@gmail.com>
2023-03-01 22:20:28 +0300

    Create rep OOP

---
+++ src/main/java/org/example/Seminar_1/HW01/Main.java
@@ -0,0 +1,7 @@
+package org.example.Seminar_1.HW01;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/org/example/Seminar_1/Task01.java
@@ -0,0 +1,7 @@
+package org.example.Seminar_1;
+
+public class Task01 {
+    public static void main(String[] args) {
+
+    }
+}
